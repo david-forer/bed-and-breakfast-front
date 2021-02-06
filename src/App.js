5fc457bf83c8cca815bd2126d8f968b1e9890a23
david-forer <david.j.forer@gmail.com>
2021-02-06 23:33:24 +0900

    added login and logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Login from './components/Login';
+import { getCurrentUser } from "./actions/currentUser.js";
+import { connect } from 'react-redux'
+import Logout from './components/Logout';
 
 class App extends React.Component {
 
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
 
   render() {
     return (
      
-      <div className="App">
-        <h1> Hello World!</h1>
-         
-      </div>
-    )
+      // <div className="App">
+      //   <h1> Hello World!</h1>
+        this.props.currentUser ? <Logout /> : <Login />
+      // </div>
+    );
   }
 }
 
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
 
-export default App;
+export default connect(mapStateToProps, {getCurrentUser} ) (App);
