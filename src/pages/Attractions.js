1d09b37768d8a9ebca026e95d5759a96c579f639
david-forer <david.j.forer@gmail.com>
2021-02-08 11:23:50 +0900

    added pages to project

---
+++ src/pages/Attractions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Attractions = () => {
+    return (
+        <div>
+           <h1>Attractions Page</h1> 
+        </div>
+    )
+}
+
+export default Attractions
---
+++ src/pages/Cafe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cafe = () => {
+    return (
+        <div>
+            <h1> Cafe Page</h1>
+        </div>
+    )
+}
+
+export default Cafe
