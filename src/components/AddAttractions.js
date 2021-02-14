57aaa55f498aba62a87ec8b33b1a48a5d132a4b7
david-forer <david.j.forer@gmail.com>
2021-02-14 13:00:11 +0900

    styled the add atractions page

--- src/components/AddAttractions.js
+++ src/components/AddAttractions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import {addAttraction} from '../actions/addAttractions'
+import { addAttraction } from "../actions/addAttractions";
 
 class AttractionInput extends React.Component {
   state = {
@@ -29,34 +29,49 @@ class AttractionInput extends React.Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>Attraction Name: </label>
+          <div className="text-center">
+          <div className="xl:w-full border-b border-gray-300 py-5 mt-20">
+            <div className="flex items-center w-11/12 mx-auto">
+              <p className="text-lg text-gray-800 font-bold">
+                Attraction Information
+              </p>
+              <div className="ml-2 cursor-pointer text-gray-600 "></div>
+            </div>
+          </div>
+          <div className="flex-column mx-4 mt-10">
+          <label className="pb-2 text-sm font-bold text-gray-800">Attraction Name: </label>
           <input
             type="text"
             placeholder="Name"
             value={this.state.name}
             name="name"
             onChange={this.handleChange}
-                />
-                <br />
-                <label>Attraction Description: </label>
+            className="w-1/2 px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 mb-4"
+          />
+          <br />
+          <label  className="pb-2 text-sm font-bold text-gray-800">Attraction Description: </label>
           <input
-            type="text"
+            type="textarea"
             placeholder="Description"
             value={this.state.description}
             name="description"
             onChange={this.handleChange}
+            className="w-1/2 px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 mb-4"
           />
           <br />
-          <label>Attraction Location: </label>
+          <label  className="pb-2 text-sm font-bold text-gray-800">Attraction Location: </label>
           <input
             type="text"
             placeholder="Location"
             value={this.state.location}
             name="location"
             onChange={this.handleChange}
+            className="w-1/2 px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
           />
           <br />
-          <input type="submit" />
+            <input className="bg-indigo-500 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white lg:w-1/4 w-1/2 py-4 text-sm focus:outline-none mt-6 " type="submit" />
+            </div>
+            </div>
         </form>
       </div>
     );
