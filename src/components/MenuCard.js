d109a28eb63a0669b59e3ae25964d11f8df92235
david-forer <david.j.forer@gmail.com>
2021-02-16 13:43:59 +0900

    styled menu card

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -2,12 +2,19 @@ import React from "react";
 
 const MenuCard = ({ menu }) => {
   return (
-      <div>
-          <br></br>
-      {menu.attributes.name}<br/>
-      {menu.attributes.desc}<br/>
-      {menu.attributes.price}
-    </div>
+    <>
+      {/* <div className="container mt-5 mx-auto px-2"> */}
+      <div className="max-w-md w-150 bg-white shadow-md rounded-lg overflow-hidden mx-auto border m-4">
+         <div className="p-4">
+      <h3 className="text-blue-800 font-bold">{menu.attributes.name}</h3>
+      <p>{menu.attributes.desc}</p><br/>
+         <p> The Cost is: ${menu.attributes.price} yen</p> <br/>
+            <p className="text-red-500">Stock: {menu.attributes.status}</p>
+            {/* <img src="{menu.attributes.image}" alt={menu.name }/> */}
+          </div> 
+        </div>
+        {/* </div> */}
+      </>
   );
 };
 
